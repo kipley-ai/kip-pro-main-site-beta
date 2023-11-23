6bfcedbfa6a4c050bc155e6e43d3cc77ba8da07a
ridho-lexikat <ridho@lexikat.com>
2023-11-23 18:26:02 +0700

    add team images

--- templates/HomePage/Statistics/index.tsx
+++ templates/HomePage/Statistics/index.tsx
@@ -6,22 +6,20 @@ import { statistics } from "@/mocks/statistics";
 type StatisticsProps = {};
 
 const Statistics = ({}: StatisticsProps) => (
-    <div className={cn("section", styles.section)}>
-        <div className={cn("container-large", styles.container)}>
-            <div className={styles.wrapper}>
-                <div className={styles.list}>
-                    {statistics.map((item, index) => (
-                        <div className={styles.item} key={index}>
-                            <div className={cn("h3", styles.value)}>
-                                {item.value}
-                            </div>
-                            <div className={styles.title}>{item.title}</div>
-                        </div>
-                    ))}
-                </div>
+  <div className={cn("section", styles.section)}>
+    <div className={cn("container-large", styles.container)}>
+      <div className={styles.wrapper}>
+        <div className={styles.list}>
+          {statistics.map((item, index) => (
+            <div className={styles.item} key={index}>
+              <div className={cn("h3", styles.value)}>{item.value}</div>
+              <div className={styles.title}>{item.title}</div>
             </div>
+          ))}
         </div>
+      </div>
     </div>
+  </div>
 );
 
 export default Statistics;
