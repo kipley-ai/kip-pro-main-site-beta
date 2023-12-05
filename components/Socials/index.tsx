ef44d9687f6a6a1ea9b268c5adf572eb87f8fb23
ridho-lexikat <ridho@lexikat.com>
2023-12-05 19:05:28 +0700

    change logo and add galxe

--- components/Socials/index.tsx
+++ components/Socials/index.tsx
@@ -1,46 +1,51 @@
 import cn from "classnames";
 import styles from "./Socials.module.sass";
 import Icon from "@/components/Icon";
+import Image from "@/components/Image";
 
 type SocialsType = {
-    icon: string;
-    href: string;
+  icon: string;
+  href: string;
 };
 
 type SocialsProps = {
-    className?: string;
-    socialClassName?: string;
-    socials: SocialsType[];
-    dark?: boolean;
-    large?: boolean;
+  className?: string;
+  socialClassName?: string;
+  socials: SocialsType[];
+  dark?: boolean;
+  large?: boolean;
 };
 
 const Socials = ({
-    className,
-    socialClassName,
-    socials,
-    dark,
-    large,
+  className,
+  socialClassName,
+  socials,
+  dark,
+  large,
 }: SocialsProps) => (
-    <div
-        className={cn(
-            styles.socials,
-            { [styles.dark]: dark, [styles.large]: large },
-            className
+  <div
+    className={cn(
+      styles.socials,
+      { [styles.dark]: dark, [styles.large]: large },
+      className
+    )}
+  >
+    {socials.map((social, index) => (
+      <a
+        className={cn(styles.social, socialClassName)}
+        href={social.href}
+        target="_blank"
+        rel="noreferrer"
+        key={index}
+      >
+        {social.icon === "galxe" ? (
+          <Image src="/02_Galxe_B_ Symbol_White.png" width={24} height={24} />
+        ) : (
+          <Icon name={social.icon} />
         )}
-    >
-        {socials.map((social, index) => (
-            <a
-                className={cn(styles.social, socialClassName)}
-                href={social.href}
-                target="_blank"
-                rel="noreferrer"
-                key={index}
-            >
-                <Icon name={social.icon} />
-            </a>
-        ))}
-    </div>
+      </a>
+    ))}
+  </div>
 );
 
 export default Socials;
--- constants/socials.tsx
+++ constants/socials.tsx
@@ -1,22 +1,22 @@
 export const socials = [
-    {
-        icon: "discord",
-        href: "https://discord.gg/eKp45C8G8K",
-    },
-    {
-        icon: "x",
-        href: "https://x.com/KIPprotocol",
-    },
-    // {
-    //     icon: "instagram",
-    //     href: "https://www.instagram.com/ui8net/",
-    // },
-    // {
-    //     icon: "telegram",
-    //     href: "https://ui8.net/",
-    // },
-    // {
-    //     icon: "facebook",
-    //     href: "https://www.facebook.com/ui8.net/",
-    // },
+  {
+    icon: "discord",
+    href: "https://discord.gg/eKp45C8G8K",
+  },
+  {
+    icon: "x",
+    href: "https://x.com/KIPprotocol",
+  },
+  {
+    icon: "galxe",
+    href: "https://galxe.com/muBnuTr6qGaT4QXifPbrpX",
+  },
+  // {
+  //     icon: "telegram",
+  //     href: "https://ui8.net/",
+  // },
+  // {
+  //     icon: "facebook",
+  //     href: "https://www.facebook.com/ui8.net/",
+  // },
 ];
