f7033d3b2b1659280feb933c0bc64661c538656a
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2023-12-20 14:50:42 +0700

    Fix join link bug

--- templates/HomePage/Quests/index.tsx
+++ templates/HomePage/Quests/index.tsx
@@ -6,42 +6,43 @@ import ImageGallery from "./ImageGallery";
 import Socials from "@/components/Socials";
 
 const socials = [
-  {
-    icon: "discord",
-    href: "https://discord.gg/Hma2Y6RgND",
-  },
-  {
-    icon: "x",
-    href: "https://x.com/KIPprotocol",
-  },
+    {
+        icon: "discord",
+        href: "https://discord.gg/Hma2Y6RgND",
+    },
+    {
+        icon: "x",
+        href: "https://x.com/KIPprotocol",
+    },
 ];
 
 type QuestsProps = {
-  scrollToRef: any;
+    scrollToRef: any;
 };
 
 const Quests = ({ scrollToRef }: QuestsProps) => (
-  <>
-    <div className={cn("anchor", styles.anchor)} ref={scrollToRef}></div>
-    <div className={cn("section", styles.section)}>
-      <ImageGallery />
-      <div className={styles.wrap}>
-        <div className={cn("content", styles.content)}>
-          <a
-            href={"https://galxe.com/KIPProtocol/campaign/GCQH3tUYcq"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={cn("h2", styles.join)}>
-              Join our Community Campaigns on Galxe
+    <>
+        <div className={cn("anchor", styles.anchor)} ref={scrollToRef}></div>
+        <div className={cn("section", styles.section)}>
+            <ImageGallery />
+            <div className={styles.wrap}>
+                <div className={cn("content", styles.content)}>
+                    <a
+                        href={
+                            "https://galxe.com/KIPProtocol/campaign/GCQH3tUYcq"
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cn("h2", styles.join)}
+                    >
+                        Join our Community Campaigns on Galxe
+                    </a>
+                    <h3>And follow us on</h3>
+                    <Socials className={styles.socials} socials={socials} />
+                </div>
             </div>
-          </a>
-          <h3>And follow us on</h3>
-          <Socials className={styles.socials} socials={socials} />
         </div>
-      </div>
-    </div>
-  </>
+    </>
 );
 
 export default Quests;
