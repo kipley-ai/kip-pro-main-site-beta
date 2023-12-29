cc8045bf43718a46fec6984f2458c6bcb8691e6c
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2023-12-29 11:14:45 +0700

    Add tasks countdown

--- templates/InvitationPage/Tasks/index.tsx
+++ templates/InvitationPage/Tasks/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import cn from "classnames";
 import styles from "./Tasks.module.sass";
 import Image from "@/components/Image";
@@ -25,86 +26,109 @@ const careers = [
         status: [],
         url: "https://galxe.com/KIPProtocol/campaign/GCQH3tUYcq",
     },
-    // {
-    //     title: "Web3 Developer",
-    //     content:
-    //         "Stay current on the latest Onix project developments, news, and content, updated daily.",
-    //     image: "/images/blog/photo-2.png",
-    //     status: ["REMOTE", "CONTRACT"],
-    //     url: "https://ui8.net/",
-    // },
-    // {
-    //     title: "Project Manager",
-    //     content:
-    //         "Stay current on the latest Onix project developments, news, and content, updated daily.",
-    //     image: "/images/blog/photo-3.png",
-    //     status: ["REMOTE", "FULL-TIME"],
-    //     url: "https://ui8.net/",
-    // },
 ];
-
-const days = 2;
-const hours = 12;
-const minutes = 30;
 
 type TasksProps = {};
 
-const Tasks = ({}: TasksProps) => (
-    <div className={cn("section", styles.section)}>
-        <div className={cn("container", styles.container)}>
-            <div className={cn("h2", styles.countTitle)}>
-                You have <span className={styles.timeCount}>{days}</span> days{" "}
-                <span className={styles.timeCount}>{hours}</span> hours{" "}
-                <span className={styles.timeCount}>{minutes}</span> mins to
-                complete these tasks
-            </div>
-            {careers.map((item, index) => (
-                <div className={styles.item} key={index}>
-                    <div className={styles.details}>
-                        <div className={styles.line}>
-                            {item.status.map((status, index) => (
-                                <div
-                                    className={cn(
-                                        {
-                                            "status-pink": status === "REMOTE",
-                                            "status-green":
-                                                status === "FULL-TIME",
-                                            "status-yellow":
-                                                status === "CONTRACT",
-                                        },
-                                        styles.status,
-                                    )}
-                                    key={index}
-                                >
-                                    {status}
-                                </div>
-                            ))}
-                        </div>
-                        <div className={cn("h3", styles.title)}>
-                            {item.title}
-                        </div>
-                        <div className={styles.content}>{item.content}</div>
-                        <a
-                            className={styles.apply}
-                            href={item.url}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Explore now <Icon name="arrow-right" size="26" />
-                        </a>
-                    </div>
-                    <div className={styles.preview}>
-                        <Image
-                            src={item.image}
-                            layout="fill"
-                            objectFit="cover"
-                            alt="Careers"
-                        />
-                    </div>
+const Tasks = ({}: TasksProps) => {
+    const [days, setDays] = useState<number>(0);
+    const [hours, setHours] = useState<number>(0);
+    const [minutes, setMinutes] = useState<number>(0);
+
+    const targetTime: string = "2024-01-01T00:00:00Z";
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const currentTime = new Date().getTime();
+            const targetDate = new Date(targetTime).getTime();
+
+            if (currentTime >= targetDate) {
+                setDays(0);
+                setHours(0);
+                setMinutes(0);
+                clearInterval(intervalId);
+            } else {
+                const timeRemaining = targetDate - currentTime;
+                const remainingDays = Math.floor(
+                    timeRemaining / (1000 * 60 * 60 * 24),
+                );
+                const remainingHours = Math.floor(
+                    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+                );
+                const remainingMinutes = Math.floor(
+                    (timeRemaining % (1000 * 60 * 60)) / (1000 * 60),
+                );
+
+                setDays(remainingDays);
+                setHours(remainingHours);
+                setMinutes(remainingMinutes);
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div className={cn("section", styles.section)}>
+            <div className={cn("container", styles.container)}>
+                <div className={cn("h2", styles.countTitle)}>
+                    You have <span className={styles.timeCount}>{days}</span>{" "}
+                    days <span className={styles.timeCount}>{hours}</span> hours{" "}
+                    <span className={styles.timeCount}>{minutes}</span> mins to
+                    complete these tasks
                 </div>
-            ))}
+                {careers.map((item, index) => (
+                    <div className={styles.item} key={index}>
+                        <div className={styles.details}>
+                            <div className={styles.line}>
+                                {item.status.map((status, index) => (
+                                    <div
+                                        className={cn(
+                                            {
+                                                "status-pink":
+                                                    status === "REMOTE",
+                                                "status-green":
+                                                    status === "FULL-TIME",
+                                                "status-yellow":
+                                                    status === "CONTRACT",
+                                            },
+                                            styles.status,
+                                        )}
+                                        key={index}
+                                    >
+                                        {status}
+                                    </div>
+                                ))}
+                            </div>
+                            <div className={cn("h3", styles.title)}>
+                                {item.title}
+                            </div>
+                            <div className={styles.content}>{item.content}</div>
+                            <a
+                                className={styles.apply}
+                                href={item.url}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Explore now{" "}
+                                <Icon name="arrow-right" size="26" />
+                            </a>
+                        </div>
+                        <div className={styles.preview}>
+                            <Image
+                                src={item.image}
+                                layout="fill"
+                                objectFit="cover"
+                                alt="Careers"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Tasks;
