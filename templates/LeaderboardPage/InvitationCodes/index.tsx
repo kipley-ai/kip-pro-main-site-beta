4dc91d725038ba0333f5ecc97fa3d3f2b743f40e
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-01-03 17:36:00 +0700

    Add back to campaigns button

--- templates/LeaderboardPage/InvitationCodes/index.tsx
+++ templates/LeaderboardPage/InvitationCodes/index.tsx
@@ -4,6 +4,7 @@ import styles from "./InvitationCodes.module.sass";
 import Image from "@/components/Image";
 import Item from "./Item";
 import LeaderboardTable from "../Leaderboard/LeaderboardTable";
+import Icon from "@/components/Icon";
 
 import { details } from "@/constants/detailsTechnology";
 import { leaderboardAccounts } from "@/mocks/leaderboard";
@@ -14,24 +15,34 @@ type InvitationCodesProps = {
 
 const InvitationCodes = ({ scrollToRef }: InvitationCodesProps) => (
     <div className={cn("section", styles.section)}>
-        <div className={cn("anchor", styles.anchor)} ref={scrollToRef}></div>
+        {/* <div className={cn("anchor", styles.anchor)} ref={scrollToRef}></div> */}
         <div className={cn("container", styles.container)}>
-            {details.map((x, index) => (
-                <Item
-                    className={styles.item}
-                    itemWrapClass={styles.wrap}
-                    item={x}
-                    key={index}
-                />
-            ))}
+            <div className={styles.backArrow}>
+                <a
+                    className={styles.apply}
+                    href={"/campaigns"}
+                >
+                    <Icon name="arrow-right" size="26" /> Back to Campaigns
+                </a>
+            </div>
+            <div className={styles.firstRow}>
+                {details.map((x, index) => (
+                    <Item
+                        className={styles.item}
+                        itemWrapClass={styles.wrap}
+                        item={x}
+                        key={index}
+                    />
+                ))}
 
-            <div className={styles.leaderboard}>
-                <div className={cn("h2", styles.title)}>Leaderboard</div>
-                <div className={cn("p", styles.subtitle)}>
-                    Follow us on Twitter today and experience the power of
-                    Knowledge in your life!
+                <div className={styles.leaderboard}>
+                    <div className={cn("h2", styles.title)}>Leaderboard</div>
+                    <div className={cn("p", styles.subtitle)}>
+                        Follow us on Twitter today and experience the power of
+                        Knowledge in your life!
+                    </div>
+                    <LeaderboardTable users={leaderboardAccounts} />
                 </div>
-                <LeaderboardTable users={leaderboardAccounts} />
             </div>
         </div>
     </div>
