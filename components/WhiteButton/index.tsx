2a0c24ff5b7254f0c450cb56d61b8d2088a9d3af
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-01-05 16:40:55 +0700

    Make white button text into one line

--- components/WhiteButton/index.tsx
+++ components/WhiteButton/index.tsx
@@ -7,7 +7,7 @@ const WhiteButton = ({ }: WhiteButtonProps) => (
     <div className={styles.line}>
         <div className={styles.backArrow}>
             <a className={styles.apply} href={"/leaderboard"}>
-                INVITE CODE & LEADERBOARDS <Icon name="arrow-right" size="26" />
+                INVITE CODE & LEADERBOARD <Icon name="arrow-right" size="26" />
             </a>
         </div>
     </div>
