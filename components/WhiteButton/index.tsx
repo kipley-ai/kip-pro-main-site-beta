1bfb9de16b4cea82c2371d69c331e506c2ee9a31
Yosafat Adi Christian Putra <yoschristian08@gmail.com>
2024-01-05 16:32:59 +0700

    modified White Button
    changed the text to INVITE CODE & LEADERBOARDS and make the text in one line (no wrap)

--- components/WhiteButton/index.tsx
+++ components/WhiteButton/index.tsx
@@ -3,11 +3,11 @@ import styles from "./WhiteButton.module.sass";
 
 type WhiteButtonProps = {};
 
-const WhiteButton = ({}: WhiteButtonProps) => (
+const WhiteButton = ({ }: WhiteButtonProps) => (
     <div className={styles.line}>
         <div className={styles.backArrow}>
             <a className={styles.apply} href={"/leaderboard"}>
-                VIEW LEADERBOARD <Icon name="arrow-right" size="26" />
+                INVITE CODE & LEADERBOARDS <Icon name="arrow-right" size="26" />
             </a>
         </div>
     </div>
