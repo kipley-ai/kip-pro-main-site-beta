d460f08931aa440b0a9fbd0d81a6252bb146d200
Kipley.ai <139740410+kipley-ai@users.noreply.github.com>
2024-01-08 22:54:30 +0800

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -41,11 +41,6 @@ const Header = ({}: HeaderProps) => {
           <Logo className={styles.logo} />
           {pathname !== "/invite" ? (
             <div className={styles.links}>
-              <Link href="/campaigns">
-                <a className={cn("a", styles.codeLink)}>
-                  <span className={styles["link-text"]}>Join Campaigns Now</span>
-                </a>
-              </Link>
               <Link href="/about-us">
                 <a className={cn("a", styles.link)}>
                   <span className={styles["link-text"]}>About Us</span>
