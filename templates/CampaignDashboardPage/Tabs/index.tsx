e44cf9005c03440faa16ac2fc73b75042160e237
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-01-15 10:05:09 +0700

    Change hover and active tab visual

--- templates/CampaignDashboardPage/Tabs/index.tsx
+++ templates/CampaignDashboardPage/Tabs/index.tsx
@@ -28,15 +28,30 @@ const TabsHeader = ({ tab, setTab }: TabsHeaderProps) => {
                 BACK TO GENESIS CAMPAIGN
             </div>
             <div className={styles.tabs}>
-                <div className={styles.tab} onClick={() => setTab("invites")}>
+                <div
+                    className={`${styles.tab} ${
+                        tab === "invites" ? styles.active : ""
+                    }`}
+                    onClick={() => setTab("invites")}
+                >
                     <h1>Invites</h1>
                     <div className={styles.ind}></div>
                 </div>
-                <div className={styles.tab} onClick={() => setTab("leaderboard")}>
+                <div
+                    className={`${styles.tab} ${
+                        tab === "leaderboard" ? styles.active : ""
+                    }`}
+                    onClick={() => setTab("leaderboard")}
+                >
                     <h1>Leaderboard</h1>
                     <div className={styles.ind}></div>
                 </div>
-                <div className={styles.tab} onClick={() => setTab("nftcollection")}>
+                <div
+                    className={`${styles.tab} ${
+                        tab === "nftcollection" ? styles.active : ""
+                    }`}
+                    onClick={() => setTab("nftcollection")}
+                >
                     <h1>NFT Collection</h1>
                     <div className={styles.ind}></div>
                 </div>
