1055d8d52f41240287fb7c497bef60900e11cbb9
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-01-15 20:41:49 +0700

    Redirect /blog to blog.kip.pro

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://blog.kip.pro/',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
