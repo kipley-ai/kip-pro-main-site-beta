80efbeb322c901f1d032faf4a6a39d25750e9474
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-01-16 14:06:06 +0700

    Change /leaderboard to CampaignDashboardPage

--- pages/campaign-dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import CampaignDashboardPage from "@/templates/CampaignDashboardPage";
-
-const Home: NextPage = () => {
-    return <CampaignDashboardPage />;
-};
-
-export default Home;
