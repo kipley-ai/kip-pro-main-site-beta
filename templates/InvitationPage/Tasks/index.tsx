b46103ba41ab219db7cf7eaa0ff62ded25318895
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-02-07 14:25:34 +0700

    Change cycle to cycle 8

--- templates/InvitationPage/Tasks/index.tsx
+++ templates/InvitationPage/Tasks/index.tsx
@@ -11,7 +11,7 @@ import { relative } from "path";
 
 const posts = [
     {
-        title: "Cycle 7 Tasks",
+        title: "Cycle 8 Tasks",
         date: "", 
         // image: CampaignImage,
         status: "FEATURES",
@@ -37,7 +37,7 @@ const Tasks = ({}: TasksProps) => {
     const [hours, setHours] = useState<number>(0);
     const [minutes, setMinutes] = useState<number>(0);
 
-    const targetTime: string = "2024-02-05T03:00:00Z";
+    const targetTime: string = "2024-02-11T03:00:00Z";
 
     useEffect(() => {
         const intervalId = setInterval(() => {
