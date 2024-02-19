3688b74de45b28e9b633b17068ccd9d7abec775f
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-02-19 11:51:48 +0700

    Change cycle to cycle 10

--- templates/InvitationPage/Tasks/index.tsx
+++ templates/InvitationPage/Tasks/index.tsx
@@ -11,7 +11,7 @@ import { relative } from "path";
 
 const posts = [
     {
-        title: "Cycle 9 Tasks",
+        title: "Cycle 10 Tasks",
         date: "", 
         // image: CampaignImage,
         status: "FEATURES",
@@ -37,7 +37,7 @@ const Tasks = ({}: TasksProps) => {
     const [hours, setHours] = useState<number>(0);
     const [minutes, setMinutes] = useState<number>(0);
 
-    const targetTime: string = "2024-02-15T12:00:00Z";
+    const targetTime: string = "2024-02-22T04:00:00Z";
 
     useEffect(() => {
         const intervalId = setInterval(() => {
